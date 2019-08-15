85900341f681f56e622a0faabae566d82159ae74
m4rkhenderson <mhenderson@upei.ca>
2019-08-15 17:41:02 -0300

    Added and modified code for controlling eddie platform.

--- robot_odometry/src/eddie_odom.cpp
+++ robot_odometry/src/eddie_odom.cpp
@@ -7,39 +7,74 @@
 */
 #include <ros/ros.h>
 #include <geometry_msgs/Vector3.h>
+#include <std_msgs/String.h>
+#include <std_msgs/Empty.h>
 #include <tf/transform_broadcaster.h>
 #include <nav_msgs/Odometry.h>
 #include <sensor_msgs/JointState.h>
-#include <morelab_robot_platform/DistanceWheel.h>
+#include <serial/serial.h>
+//#include <morelab_robot_platform/DistanceWheel.h>
 //
-double cur_left_dis = 0.0;
-double cur_right_dis = 0.0;
-double old_left_dis = 0.0;
-double old_right_dis = 0.0;
+serial::Serial ser;
+std::__cxx11::string enc;
+int cur_left_dis = 0;
+int cur_right_dis = 0;
+int old_left_dis = 0;
+int old_right_dis = 0;
+const char encReq[5] = {'D','I','S','T','\r'};
+char encL[4];
+char encR[4];
+
 //
-void distanceWheelCallback(const morelab_robot_platform::DistanceWheel::ConstPtr& message)
-{
+//void distanceWheelCallback(const morelab_robot_platform::DistanceWheel::ConstPtr& message)
+//{
 
-    if (message->status.substr(0, 5) == "ERROR") // ERROR messages may be longer than 5 if in VERBOSE mode
-    {
-        ROS_ERROR("ERROR: Unable to read distance wheel data from encoders");
-        return;
-    }
-    //cur_left_dis = 0 - message->value[0];
-    //cur_right_dis = 0 - message->value[1];
+//    if (message->status.substr(0, 5) == "ERROR") // ERROR messages may be longer than 5 if in VERBOSE mode
+//    {
+//        ROS_ERROR("ERROR: Unable to read distance wheel data from encoders");
+//        return;
+//    }
+//    //cur_left_dis = 0 - message->value[0];
+//    //cur_right_dis = 0 - message->value[1];
 
-    cur_left_dis = 0 - message->value[1];
-    cur_right_dis = 0 - message->value[0];
-    ROS_INFO("Odom: left = %f, right = %f", cur_left_dis, cur_right_dis);
+//    cur_left_dis = 0 - message->value[1];
+//    cur_right_dis = 0 - message->value[0];
+//    ROS_INFO("Odom: left = %f, right = %f", cur_left_dis, cur_right_dis);
 
-}
+//}
 //
 int main(int argc, char** argv){
   ros::init(argc, argv, "eddie_odom");
 
   ros::NodeHandle n;
   ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("odom", 10);
-  ros::Subscriber distance_wheel_sub = n.subscribe("/eddie/distance_wheel", 10, distanceWheelCallback);
+//  ros::Subscriber distance_wheel_sub = n.subscribe("/eddie/distance_wheel", 10, distanceWheelCallback);
+
+  try
+  {
+      ser.setPort("/dev/ttyUSB0");
+      ser.setBaudrate(115200);
+      ser.setBytesize(serial::eightbits);
+      ser.setParity(serial::parity_none);
+      ser.setStopbits(serial::stopbits_one);
+      ser.setFlowcontrol(serial::flowcontrol_none);
+      serial::Timeout to = serial::Timeout::simpleTimeout(1000);
+      ser.setTimeout(to);
+      ser.open();
+      ser.setRTS(true);
+      ser.setDTR(true);
+  }
+  catch (serial::IOException& e)
+  {
+      ROS_ERROR_STREAM("Unable to open port ");
+      return -1;
+  }
+
+  if(ser.isOpen()){
+      ROS_INFO_STREAM("Serial Port initialized");
+  }else{
+      return -1;
+  }
 
   double sticks_meter = 405; // The number of wheel encoder ticks per meter of travel
   double base_width = 0.39; // distance between the center of two wheels
@@ -80,10 +115,26 @@ int main(int argc, char** argv){
     //compute odometry in a typical way given the velocities of the robot
     double dt = (current_time - last_time).toSec();
 
+    //request the current encoder values
+    ser.write(encReq);
+    ROS_INFO("REQUESTED ENCODER MEASUREMENTS");
+    if(ser.available() > 7){
+      enc = ser.read(ser.available());
+      for(int i=0; i<4;i++){
+          encR[i] = enc[3-i];
+      }
+      for(int i=0; i<4;i++){
+          encL[i] = enc[7-i];
+      }
+      cur_left_dis = *(int *)encL;
+      cur_right_dis = *(int *)encR;
+      ROS_INFO("OBTAINED ENCODER MEASUREMENTS: %d, %d", cur_left_dis, cur_right_dis);
+    }
+
     // traveled distances for the left and right wheel
-    d_left = (0.495* (cur_left_dis - old_left_dis)/144);
-    d_right = (0.495* (cur_right_dis - old_right_dis)/144);
-    ROS_INFO("d_left = %f, d_right = %f", d_left, d_right);
+    d_left = (0.495* (cur_left_dis - old_left_dis)/2800);
+    d_right = (0.495* (cur_right_dis - old_right_dis)/2800);
+    //ROS_INFO("d_left = %f, d_right = %f", d_left, d_right);
 
     old_left_dis = cur_left_dis;
     old_right_dis = cur_right_dis;
@@ -95,7 +146,7 @@ int main(int argc, char** argv){
     delta_th = (d_right - d_left) / base_width;
     delta_x = (delta_s * cos(th + delta_th/2));
     delta_y = (delta_s * sin(th + delta_th/2));
-    ROS_INFO("delta_x = %f, delta_y = %f, delta_th = %f", delta_x, delta_y, delta_th);
+    //ROS_INFO("delta_x = %f, delta_y = %f, delta_th = %f", delta_x, delta_y, delta_th);
 
 
     // calculate velocities
@@ -104,7 +155,7 @@ int main(int argc, char** argv){
     vth = delta_th/dt;
     v = delta_s/dt;
 
-    ROS_INFO("x = %f, y = %f, th = %f", x, y, th);
+    //ROS_INFO("x = %f, y = %f, th = %f", x, y, th);
     x += delta_x;
     y += delta_y;
     th += delta_th;
